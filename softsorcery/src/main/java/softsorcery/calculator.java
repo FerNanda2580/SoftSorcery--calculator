3c3d01e6bef91c2d5d4f2ffc5967ffd09d5b861f
D-Ronald <danielronald21@gmail.com>
2024-04-19 14:26:02 -0300

    fatorial e potencia + testes

--- softsorcery/src/main/java/softsorcery/calculator.java
+++ softsorcery/src/main/java/softsorcery/calculator.java
@@ -21,9 +21,40 @@ public class calculator {
         return a / b;
     }
  
-    //int potenciacao(int a, int b){ 
-    //}
+    float potenciacao(int a, int b){ 
+        if (b == 0){
+            return 1;
+        }
+        if(b < 0 ){
+            int potenciacao = a;
+            for(int i= 0; i<(-b-1); i++ ){
+            potenciacao = a*potenciacao;
+            }
+            float numerador = 1;
+            float result = numerador/potenciacao;
+            return result;
+        }
+        int potenciacao = a;
+        for(int i= 0; i<b-1; i++ ){
+            potenciacao = a*potenciacao;
+        }
+        return potenciacao;
+        }
+
+        
+    
  
-    //int raizQuadrada(double a) {
-    //}
+    int fatorial(int a) {
+        if( a < 0){
+            throw new IllegalArgumentException("Numeros negativos não possuem fatorial");
+        }
+        else if(a == 0){
+            return 1;
+        }
+        int resultado = 1;
+        for(int i = a; i>=1; i--){
+            resultado *= i;
+        }
+        return resultado;
+    }
 }
--- softsorcery/src/test/java/softsorcery/calculatorTest.java
+++ softsorcery/src/test/java/softsorcery/calculatorTest.java
@@ -27,12 +27,34 @@ public class calculatorTest {
         assertEquals(-6, calc.multiplicacao(2, -3));
         assertEquals(6, calc.multiplicacao(-2, -3));
     }
+
     @Test
     public void divisaoPorZero() {
-        calculator calc = new calculator(); 
         assertThrows(IllegalArgumentException.class, () -> {
             calc.divisao(6, 0);
         });
         assertThrows(IllegalArgumentException.class, () -> calc.divisao(6, 0));
     }
+
+    @Test
+    public void potenciacaoTest(){
+        assertEquals(1024, calc.potenciacao(2, 10));
+        assertEquals(1, calc.potenciacao(0, 0));
+        assertEquals(-8, calc.potenciacao(-2,3));
+        assertEquals(0, calc.potenciacao(0, 1));
+        assertEquals(1, calc.potenciacao(10, 0));
+        assertEquals(0.0625, calc.potenciacao(2, -4));
+    }
+
+    @Test
+    public void fatorialTest() {
+        assertEquals(1, calc.fatorial(0));
+        assertEquals(1, calc.fatorial(1));
+        assertEquals(2, calc.fatorial(2));
+        assertEquals(24, calc.fatorial(4));
+        assertEquals(120, calc.fatorial(5));
+        assertThrows(IllegalArgumentException.class, () -> {
+            calc.fatorial(-1);
+        });
+}   
 }
