39bb6cb3409b409f1741bcd45049b96966b701ce
raynayara <raysllannayara10@gmail.com>
2024-04-19 12:45:42 -0300

    multiplication/division

--- softsorcery/src/test/java/softsorcery/calculatorTest.java
+++ softsorcery/src/test/java/softsorcery/calculatorTest.java
@@ -2,40 +2,37 @@ package softsorcery;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class calculatorTest {
     calculator calc = new calculator();
+
     @Test
-    public void soma(){
+    public void soma() {
         assertEquals(5, calc.add(2, 3));
         assertEquals(-1, calc.add(2, -3));
         assertEquals(-4, calc.add(-2, -2));
     }
 
     @Test
-    public void subtracao(){
+    public void subtracao() {
         assertEquals(-1, calc.subtract(2, 03));
         assertEquals(0, calc.subtract(-3, -3));
         assertEquals(-5, calc.subtract(-2, 03));
     }
 
     @Test
-    public void multiplicacao(){
-        assertEquals(6, calc.multiply(2, 3));
-        assertEquals(-6, calc.multiply(2, -3));
-        assertEquals(6, calc.multiply(-2, -3));
+    public void multiplicacao() {
+        assertEquals(6, calc.multiplicacao(2, 3));
+        assertEquals(-6, calc.multiplicacao(2, -3));
+        assertEquals(6, calc.multiplicacao(-2, -3));
     }
-
-    @Test ()
-    public void divisao(){
-        assertEquals(2, calc.divide(6, 3));
-        assertEquals(-2, calc.divide(6, -3));
-        assertEquals(2, calc.divide(-6, -3));
-    }
-
-    @Test (expected = IllegalArgumentException.class)
-    public void divisaoPorZero(){
-        calc.divide(6, 0);
+    @Test
+    public void divisaoPorZero() {
+        calculator calc = new calculator(); 
+        assertThrows(IllegalArgumentException.class, () -> {
+            calc.divisao(6, 0);
+        });
+        assertThrows(IllegalArgumentException.class, () -> calc.divisao(6, 0));
     }
 }
-
